9717f2bb0c31661658af1f47828df88968cdd995
Swetha S <110710815+Swetha5021@users.noreply.github.com>
2023-10-04 22:23:15 +0530

    Update Queue.cpp

--- FOP IN C++/1D Array/Queue.cpp
+++ FOP IN C++/1D Array/Queue.cpp
@@ -1,5 +1,8 @@
 /*Queue
-A bus stop queue has n groups of people. The i-th group from the beginning has ai people. Every 30 minutes an empty bus arrives at the bus stop, it can carry at most m people. Naturally, the people from the first group enter the bus first. Then go the people from the second group and so on. Note that the order of groups in the queue never changes. Moreover, if some group cannot fit all of its members into the current bus, it waits for the next bus together with other groups standing after it in the queue. Your task is to determine the number of buses needed to transport all n groups to the Jeju Island countryside.
+A bus stop queue has n groups of people. The i-th group from the beginning has ai people. Every 30 minutes an empty bus arrives at the bus stop, it can carry
+at most m people. Naturally, the people from the first group enter the bus first. Then go the people from the second group and so on. Note that the order of 
+groups in the queue never changes. Moreover, if some group cannot fit all of its members into the current bus, it waits for the next bus together with other 
+groups standing after it in the queue. Your task is to determine the number of buses needed to transport all n groups to the Jeju Island countryside.
 Input Format:
 The first line contains two integers n and m (1 ≤ n, m ≤ 100). The next line contains n integers: a1, a2, ..., an (1 ≤ ai ≤ m).
 Output Format:
