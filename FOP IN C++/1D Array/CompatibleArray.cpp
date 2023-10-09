b9dd4ba58d6b60882131bf2798bc8a9765d7ce66
Swetha S <110710815+Swetha5021@users.noreply.github.com>
2023-10-09 19:27:23 +0530

    Update CompatibleArray.cpp

--- FOP IN C++/1D Array/CompatibleArray.cpp
+++ FOP IN C++/1D Array/CompatibleArray.cpp
@@ -1,5 +1,6 @@
 /*Compatible array
 Two arrays are said to be compatible if they are of the same size and if the ith element in the first array is greater than or equal to the ith element in the second array for all the values of i. Write a program to find whether 2 arrays are compatible or not.
+
 INPUT FORMAT:
 Input consists of 2n+2 integers. 
 The first integer corresponds to ‘n1’, the size of the first array. 
@@ -7,8 +8,10 @@ The next ‘n1’ integers correspond to the elements in the first array.
 The next (n+1) integer corresponds to 'n2', the size of the second array. 
 The last 'n2' integers correspond to the elements in the second array. 
 Assume that the maximum value of n is 15.
+
 OUTPUT FORMAT:
 Refer sample output for details.
+
 SAMPLE INPUT:
 5
 
