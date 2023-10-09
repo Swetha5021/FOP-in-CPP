e5c271c96473665bc3e234ca99e385c0b2ece5f8
Swetha S <110710815+Swetha5021@users.noreply.github.com>
2023-10-09 19:28:14 +0530

    Update ArrayInsertion.cpp

--- FOP IN C++/1D Array/ArrayInsertion.cpp
+++ FOP IN C++/1D Array/ArrayInsertion.cpp
@@ -1,7 +1,9 @@
 /* Array insertion
 Arjuna and Karna were playing cards. Arjuna has 5 cards. Karna wants to insert a new card in between Arjuna's cards. Karna wants to find the position to insert the card. So help him to find the position to insert a certain card. If Karna inserts a card in a position other than the position of 5 cards, the game will be over. So play carefully. EXAMPLE: For example, consider an array having three elements in it initially and a[0] = 1, a[1] = 2 and a[2] = 3 and you want to insert a number 45 at location 1 i.e. a[0] = 45, so we have to move elements one step below after insertion a[1] = 1 , and a[2] = 2 and a[3] = 3.
+
 Input & Output Format:
 Assume that the maximum number of elements in the array is 20.
+
 Sample Input:
 Enter the number of elements in the array
 5
@@ -15,6 +17,7 @@ Enter the location where you wish to insert an element
 4
 Enter the value to insert
 10
+
 Sample Output:
 Array after insertion is
 1
